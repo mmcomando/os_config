9ad7867b762fed0da4bc7c21108c380709420d8d
Michał Masiukiewicz <mmcomando@gmail.com>
2022-06-26 10:33:17 +0200

    Update launch terminal script

--- scripts/quake_like_terminal.py
+++ scripts/quake_like_terminal.py
@@ -1,7 +1,36 @@
 import subprocess
+import time
 
 
-out = subprocess.check_output(['swaymsg', '[app_id="terminal_scratchpad"] scratchpad show,resize set 100 ppt 30 ppt, move position 0 ppt 0 ppt'])
-print(out.decode())
- 
-  
+def create_terminal():
+    out = subprocess.run(
+        ['swaymsg', 'exec alacritty --class=terminal_scratchpad'],
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
+    return out
+
+
+def show_terminal():
+    out = subprocess.run(
+        ['swaymsg', '[app_id="terminal_scratchpad"] scratchpad show,resize set 100 ppt 30 ppt, move position 0 ppt 0 ppt'],
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
+    return out
+
+
+out = show_terminal()
+out_text = out.stdout.decode()
+if out.returncode != 0 and 'No matching node' in out_text:
+    print(out.returncode)
+    print(out_text)
+    print('Terminal is no present, create one')
+    out_terminal = create_terminal()
+    if out_terminal.returncode != 0:
+        print('Failed to create terminal')
+    else:
+        time.sleep(1)
+        out = show_terminal()
+        if out.returncode != 0:
+            print('Still failed to show terminal, something is wrong')
+            print(out.returncode)
+            print(out.stdout.decode())
